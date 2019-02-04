921c23099d7f272db9ed1c3474e47437dfbf5e01
José Oswaldo Montalvo Espinoza <jomontalvo@hotmail.com>
2019-02-03 20:49:47 -0500

    Add Camera Nugets
    Android an iOS Capabilities acces to camera for Profile Page

--- SigobMobile/SigobMobile.Android/MainActivity.cs
+++ SigobMobile/SigobMobile.Android/MainActivity.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
+[assembly: UsesFeature("android.hardware.camera", Required = false)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
 namespace SigobMobile.Droid
 {
     [Activity(Label = "SigobMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -19,7 +21,15 @@ namespace SigobMobile.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
